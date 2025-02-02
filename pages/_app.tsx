aa667664e4810fce8360981a359026807bc80706
Kotov Aleksandr <kotov.alexandr@gmail.com>
2025-02-03 00:54:39 +0300

    Create Next App with PagesRouter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from 'next/head';
+import React, { JSX } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// export default function App({ Component, pageProps }: AppProps): JSX.Element {
+//   return <Component {...pageProps} />;
+// }
+
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <>
+    <Head>
+      <title>MyTop - наш лучший топ</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
